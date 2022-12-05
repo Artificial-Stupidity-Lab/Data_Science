14a3ed983195ecbb1756fe42ebd8f5ddafdecb19
Mpilo Mbulelo Tatana <81185746+Artificial-Stupidity-Lab@users.noreply.github.com>
2022-12-05 02:06:49 +0200

    update

--- data_science_handbook.py
+++ data_science_handbook.py
@@ -1,5 +1,20 @@
 import numpy as np
 # Python code
-result = 0
+""" result = 0
 for i in range(10):
- result += i
+ print(i) """
+
+""" L = list(range(10))
+print(L)
+L2 = [str(c) for c in L]
+print(L2)
+L3 = [True, "a", 3.55, 2]
+L3 = [type(c) for c in L3]
+print(L3) """
+
+''''#creating numpy array
+arr = np.array([1,2,3])
+print(arr)'''
+
+# Create a 3x5 floating-point array filled with 1s
+np.ones((3, 5), dtype=float)
