cfd124b1f75fef40e2adfd749a621e0d65d8bb11
Mpilo Mbulelo Tatana <81185746+Artificial-Stupidity-Lab@users.noreply.github.com>
2022-12-06 13:38:53 +0200

    update

--- data_science_handbook.py
+++ data_science_handbook.py
@@ -17,4 +17,24 @@ arr = np.array([1,2,3])
 print(arr)'''
 
 # Create a 3x5 floating-point array filled with 1s
-np.ones((3, 5), dtype=float)
+#np.ones((3, 5), dtype=float)
+""" x1 = np.random.randint(200, size=6)
+print(x1) """
+""" arr1 = np.array([5,2,3,9])
+arr1[1]=1
+print(arr1) #[5 1 3 9]
+arr[-1]=7
+print(arr1) #[5 1 3 7] """
+""" arr1 = np.array([[8, 6, 9],
+                [3, 0, 8],
+                [11, 87, 100]])
+arr1[1,1] = 11
+print(arr1) # second row = [3,11,8] """
+
+name = ['Alice', 'Bob', 'Cathy', 'Doug']
+age = [25, 45, 37, 19]
+weight = [55.0, 85.5, 68.0, 61.5]
+data['name'] = name
+data['age'] = age
+data['weight'] = weight
+print(data)
